feffac2afb96bb80ac20457ade39ee366acd2c99
daanonymous12 <57075630+daanonymous12@users.noreply.github.com>
2020-07-15 17:21:58 -0400

    updated mock data
    separated price generation and time split into separate function, included option to adjust upper and lower bound

--- Mock_Data/mock_data_gen.py
+++ Mock_Data/mock_data_gen.py
@@ -11,11 +11,16 @@ class simulate:
     start and endtime should be in year-month-day-9-30 format where 9-30 is hour,minute
     """
 
-    def __init__(self,start_date,end_date,starting_price,interval,user,password,host,port,database):
+    def __init__(self,start_date,end_date,starting_price,interval,lower_limit,\
+        upper_limit,user,password,host,port,database):
         self.cursor = self.database(user,password,host,port,database)
         self.starting_price = starting_price
-        self.simulate(start_date,end_date,interval)
-        
+        self.lower = lower_limit
+        self.upper = upper_limit
+        self.simulate(self.strip(start_date),self.strip(end_date),interval)
+
+    def strip(self, time):
+        return datetime(*[int(i) for i in time.split('-')])
     def database(self,user,password,host,port,database):
         self.connection = psycopg2.connect(user = user,
                                   password = password,
@@ -24,11 +29,13 @@ class simulate:
                                   database = database)
         cursor = self.connection.cursor()
         return cursor
-
+    def price_sim(self, cur_price):
+        cur_price = (1 - (10 - random.randint(0,20))/100) * cur_price # 2% max change in price since aapl is large stock
+        if cur_price < self.lower * self.starting_price or cur_price > self.upper * self.starting_price:
+            cur_price = self.starting_price
+        return cur_price
     def simulate(self,start_date,end_date,interval):
         cur_price = self.starting_price
-        start_date = datetime(*[int(i) for i in start_date.split('-')])
-        end_date = datetime(*[int(i) for i in end_date.split('-')])
         prices = []
         minute_incre = timedelta(minutes = 60/interval)
         delta = timedelta(days = 1)
@@ -38,13 +45,12 @@ class simulate:
             if dates < 5: # weekdays
                 temp = start_date
                 while temp <= start_date + timedelta(hours = 7, minutes = 30): # interval is # of times one wish to scan per hour
-                    cur_price = (1 - (10 - random.randint(0,20))/100) * cur_price # 2% max change in price since aapl is large stock
-                    if cur_price < .6 * self.starting_price or cur_price > 3 * self.starting_price:
-                        cur_price = self.starting_price
+                    cur_price = self.price_sim(cur_price)
                     prices.append(['AAPL',dates,str(temp),cur_price])
                     temp += minute_incre
             start_date += delta
             self.writetodb(prices)
+        print('Simulation Finished')
 
     def writetodb(self,prices):
         postgres_insert_query = """ INSERT INTO stock_data (ticker,Day,Date,Price) VALUES (%s,%s,%s,%s)"""
@@ -53,4 +59,4 @@ class simulate:
         count = self.cursor.rowcount
         print (count, "Record inserted successfully into table")
 
-simulate('2019-01-01-9-30','2019-12-30-9-30',144,12,'da','','localhost','5432','da')
+simulate('2019-01-01-9-30','2019-12-30-9-30',144,12,.6,3,'da','','localhost','5432','da')
